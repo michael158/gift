5ccf2b24d42e45418128579551c43fe4744d442d
Michael Douglas <michaeldouglas@Mac-mini-de-Michael.local>
2020-10-23 11:43:51 -0300

    Updates

---
+++ precache-manifest.1b2a5275fb3586b7e5c71ea8b85acd35.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "958f4a70d7cc7024bcb1d1484db64270",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8444c36095867f43037c",
+    "url": "/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "2bfe721eea8f8dbb9b7f",
+    "url": "/static/css/main.0cb90830.chunk.css"
+  },
+  {
+    "revision": "8444c36095867f43037c",
+    "url": "/static/js/2.7c6940fd.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/static/js/2.7c6940fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bfe721eea8f8dbb9b7f",
+    "url": "/static/js/main.e8504b17.chunk.js"
+  },
+  {
+    "revision": "650e2f7f1bf4f94fd240",
+    "url": "/static/js/runtime-main.5576c0ba.js"
+  },
+  {
+    "revision": "e5813fb17fc73aa29c07762eef843875",
+    "url": "/static/media/gift.e5813fb1.pdf"
+  },
+  {
+    "revision": "6298d42165b5cfc0686e9a3c8c90f363",
+    "url": "/static/media/heart.6298d421.png"
+  }
+]);
